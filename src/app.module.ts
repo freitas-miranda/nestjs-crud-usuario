64613878d2f980def20f1135836508c3d7fb95b9
Alan Miranda <freitas.miranda@gmail.com>
2023-07-23 17:32:41 -0400

    feat: configura conexao com banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import configuration from './config/configuration';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration] })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    TypeOrmModule.forRoot({
+      type: 'mariadb',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'sql@dev',
+      database: 'nestjs_crud_usuario',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
